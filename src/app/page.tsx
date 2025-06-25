8abb843f5db9067fecbba73b776bd3c66123f045
viml kumar <vimalkg@gmail.com>
2025-06-25 09:45:07 +0530

    hscii.vercel.app hfont.vercel.app mck82.vercel.app fb.com/ztrabc x.com/etphor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ a->ram i->sijaram u->vnuman e->gnesh o->gowinq
     set_iteksta(all_output);
 }  
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
       <div className="flex gap-2 items-center flex-col sm:flex-row">
 			<Hsciifontpicker/>
